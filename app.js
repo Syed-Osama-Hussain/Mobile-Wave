6fa3670f2723178b3099c7a2cc2a30ff02d1c46a
Syed Osama Hussain <osamahussain38@gmail.com>
2019-02-03 18:38:09 +0500

    some changes

--- app.js
+++ app.js
@@ -300,10 +300,9 @@ app.get("/:device/:company",function(req,res){
 });
 
 
-app.listen("3000",function(){
-
-  console.log("The server has started");
-
+var port = process.env.PORT || 3000;
+app.listen(port, "0.0.0.0", function() {
+console.log("Listening on Port 3000");
 });
 
 
